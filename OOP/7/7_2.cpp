f59674a3d21551e74cb5833ff35a6bf02945856c
Vladimir0418 <47777552+Vladimir0418@users.noreply.github.com>
2019-04-09 13:43:52 +0300

    Add files via upload

---
+++ OOP/7/7_2.cpp
@@ -0,0 +1,21 @@
+﻿"ConsoleApplication21.exe" (Win32). Загружено "C:\Users\Skotn\source\repos\ConsoleApplication21\Debug\ConsoleApplication21.exe". Символы загружены.
+"ConsoleApplication21.exe" (Win32). Загружено "C:\Windows\SysWOW64\ntdll.dll". Невозможно найти или открыть PDB-файл.
+"ConsoleApplication21.exe" (Win32). Загружено "C:\Windows\SysWOW64\kernel32.dll". Невозможно найти или открыть PDB-файл.
+"ConsoleApplication21.exe" (Win32). Загружено "C:\Windows\SysWOW64\KernelBase.dll". Невозможно найти или открыть PDB-файл.
+"ConsoleApplication21.exe" (Win32). Загружено "C:\Windows\SysWOW64\apphelp.dll". Невозможно найти или открыть PDB-файл.
+"ConsoleApplication21.exe" (Win32). Загружено "C:\Windows\SysWOW64\msvcp140d.dll". Невозможно найти или открыть PDB-файл.
+"ConsoleApplication21.exe" (Win32). Загружено "C:\Windows\SysWOW64\vcruntime140d.dll". Невозможно найти или открыть PDB-файл.
+"ConsoleApplication21.exe" (Win32). Загружено "C:\Windows\SysWOW64\ucrtbased.dll". Невозможно найти или открыть PDB-файл.
+Поток 0x3348 завершился с кодом 0 (0x0).
+"ConsoleApplication21.exe" (Win32). Загружено "C:\Windows\SysWOW64\sechost.dll". Невозможно найти или открыть PDB-файл.
+"ConsoleApplication21.exe" (Win32). Загружено "C:\Windows\SysWOW64\rpcrt4.dll". Невозможно найти или открыть PDB-файл.
+"ConsoleApplication21.exe" (Win32). Загружено "C:\Windows\SysWOW64\sspicli.dll". Невозможно найти или открыть PDB-файл.
+"ConsoleApplication21.exe" (Win32). Загружено "C:\Windows\SysWOW64\cryptbase.dll". Невозможно найти или открыть PDB-файл.
+"ConsoleApplication21.exe" (Win32). Загружено "C:\Windows\SysWOW64\bcryptprimitives.dll". Невозможно найти или открыть PDB-файл.
+"ConsoleApplication21.exe" (Win32). Загружено "C:\Windows\SysWOW64\kernel.appcore.dll". Невозможно найти или открыть PDB-файл.
+"ConsoleApplication21.exe" (Win32). Загружено "C:\Windows\SysWOW64\msvcrt.dll". Невозможно найти или открыть PDB-файл.
+Поток 0x2a6c завершился с кодом 0 (0x0).
+Поток 0x1f84 завершился с кодом 0 (0x0).
+Поток 0xaf8 завершился с кодом 0 (0x0).
+Поток 0x3b68 завершился с кодом 0 (0x0).
+Программа "[14228] ConsoleApplication21.exe" завершилась с кодом 0 (0x0).
