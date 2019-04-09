b6aafde6b639ebb7c11a0640474435da6c612577
Vladimir0418 <47777552+Vladimir0418@users.noreply.github.com>
2019-04-09 14:08:55 +0300

    Add files via upload

---
+++ OOP/PR7/7_1.cpp
@@ -0,0 +1,78 @@
+﻿#include "pch.h"
+
+#include <iostream>
+
+#include<string>
+
+using namespace std;
+
+class Animal
+{
+protected:
+	string name;
+	int year;
+public:
+	Animal(string name, int year) {
+		this->name = name;
+		this->year = year;
+	}
+	Animal()
+	{
+		cout << "введите имя кол-во лет " << endl;
+		cin >> name;
+		cin >> year;
+	}
+	virtual void golos() = 0;
+	void get_Data() {
+		cout << "да " << name << " " << year << endl;
+	}
+};
+class Cat : public Animal {
+
+public:
+
+	Cat(string name, int year) :Animal(name, year) {}
+
+	Cat() :Animal() {}
+
+	void golos() {
+
+		cout << "мяяяууууу" << endl;
+
+	}
+
+}; 
+class Dog : public Animal {
+public:
+	Dog(string name, int year) :Animal(name, year) {}
+	Dog() :Animal() {}
+	void golos()
+	{
+		cout << "гаааввв гааавв гааавв" << endl;
+	}
+};
+class Parrot : public Animal
+{
+public:
+	Parrot(string name, int year) :Animal(name, year) {}
+	Parrot() :Animal() {}
+	void golos()
+	{
+		cout << "дорова" << endl;
+	}
+};
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	Cat a;
+	a.get_Data();
+	a.golos();
+	Dog b;
+	b.get_Data();
+	b.golos();
+	Parrot c;
+	c.get_Data();
+	c.golos();
+	system("pause");
+	return 0;
+}
