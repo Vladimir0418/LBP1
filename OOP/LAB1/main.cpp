0a4317c4876fe00528ecb7898c7651d9d008d545
Vladimir0418 <47777552+Vladimir0418@users.noreply.github.com>
2019-04-16 14:41:09 +0300

    Update main.cpp

--- OOP/LAB1/main.cpp
+++ OOP/LAB1/main.cpp
@@ -4,16 +4,17 @@
 using namespace std;
 int main()
 {
+	setlocale(LC_ALL,"russian");
 	double r, i;
-	cout << "Hello, user!\nenter your number (Re first, then Im): ";
+	cout << "введите Re ,  Im: ";
 	cin >> r >> i;
 	Complex c1(r, i);
-	cout << "\nThanks, got it! Now the second one : ";
+	cout << "\nвведите второе число : ";
 	cin >> r >> i;
 	Complex c2(r, i);
 	Complex res;
 	res = c1 + c2;
-	cout << "\nOkay, here you go, lad : ";
+	cout << "\nрезультат : ";
 	cout << "\nA + B = ";
 	res.getnum();
 	res = c1 - c2;
