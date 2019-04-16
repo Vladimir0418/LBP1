2e986247b9ea421e6924f694ef04b5c63a77b6b9
Vladimir0418 <47777552+Vladimir0418@users.noreply.github.com>
2019-04-16 14:37:26 +0300

    Add files via upload

---
+++ OOP/LAB1/compelx.h.cpp
@@ -0,0 +1,26 @@
+#pragma once
+#include "pch.h"
+#include <iostream>
+using namespace std;
+#ifndef COMPLEX_H_
+#define COMPLEX_H_
+class Complex
+{
+private:
+	double re;
+	double im;
+public:
+	Complex();
+	Complex(const double, const double);
+	void setre(const double);
+	void setim(const double);
+	double getre();
+	double getim();
+	void getnum();
+	double getmod();
+	Complex operator+ (const Complex&);
+	Complex operator- (const Complex&);
+	Complex operator* (const Complex&);
+	Complex operator/ (const Complex&);
+};
+#endif COMPLEX_H_#pragma once
---
+++ OOP/LAB1/main.cpp
@@ -0,0 +1,30 @@
+﻿#include "pch.h"
+#include "Complex.h"
+#include <iostream>
+using namespace std;
+int main()
+{
+	double r, i;
+	cout << "Hello, user!\nenter your number (Re first, then Im): ";
+	cin >> r >> i;
+	Complex c1(r, i);
+	cout << "\nThanks, got it! Now the second one : ";
+	cin >> r >> i;
+	Complex c2(r, i);
+	Complex res;
+	res = c1 + c2;
+	cout << "\nOkay, here you go, lad : ";
+	cout << "\nA + B = ";
+	res.getnum();
+	res = c1 - c2;
+	cout << "\n\nA - B = ";
+	res.getnum();
+	res = c1 * c2;
+	cout << "\n\nA * B = ";
+	res.getnum();
+	res = c1 / c2;
+	cout << "\n\nA / B = ";
+	res.getnum();
+	system("pause");
+	return 0;
+}
