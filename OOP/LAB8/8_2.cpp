53369fb33f3c4a8834b8aba119f58798f8af7b5f
Vladimir0418 <47777552+Vladimir0418@users.noreply.github.com>
2019-04-16 14:04:59 +0300

    Add files via upload

---
+++ OOP/LAB8/8_2.cpp
@@ -0,0 +1,102 @@
+﻿#include "pch.h"
+#include <iostream>
+#include <string>
+#include<cmath>
+#include<list>
+using namespace std;
+class Complex 
+{
+	double Re, Im;
+public:
+
+	Complex()
+	{
+		cout << "введите действительную часть " << endl;
+		cin >> Re;
+		cout << "введите мнимую часть" << endl;
+		cin >> Im;
+	}
+	Complex(double im, double re) 
+	{
+		this->Im = im;
+		this->Re = re;
+	}
+	Complex(const Complex &other)
+	{
+		this->Re = other.Re;
+		this->Im = other.Im;
+	}
+	~Complex() {};
+	void set_c()
+	{
+		cout << "введите действительную часть" << endl;
+		cin >> Re;
+		cout << "введите мнимую часть" << endl;
+		cin >> Im;
+	}
+	string get_c()
+	{
+		return to_string(Re) + "+" + to_string(Im) + "i";
+	}
+	double modul()
+	{
+		return sqrt(Re*Re + Im * Im);
+	}
+	double arg()
+	{
+		return atan(Im / Re);
+	}
+	friend istream& operator>> (std::istream &in, Complex &point)
+	{
+		cout << "введите действительную мнимую часть" << endl;
+		in >> point.Re;
+		in >> point.Im;
+		return in;
+	}
+	friend ostream& operator<< (std::ostream &out, const Complex &point)
+	{
+		out << point.Re << "+i*" << point.Im;
+		return out;
+	}
+	bool operator <(Complex &other)
+	{
+		if (this->modul() < other.modul())return true;
+		else return false;
+	}
+	bool operator >(Complex &other)
+	{
+		if (this->modul() > other.modul())return true;
+		else return false;
+	}
+	bool operator ==(Complex &other)
+	{
+		if (this->modul() == other.modul())return true;
+		else return false;
+	}
+	Complex& operator ++()
+	{
+		this->Re++;
+		return *this;
+	}
+	Complex& operator --()
+	{
+		this->Re--;
+		return *this;
+	}
+};
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	list<Complex> a;
+	a.push_back(Complex());
+	auto it = a.begin();
+	int z = -1;
+	do {
+		cout << "введите 1 для добавления 0-выход" << endl;
+		cin >> z;
+		if (z == 1)a.push_back(Complex());
+	} while (z != 0);
+	for (it = a.begin(); it != a.end(); ++it)cout << *it << endl;
+	system("pause");
+	return 0;
+}
